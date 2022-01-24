6202e8d175b535c45e25df0cec9cfc580199cc0f
Mojahed Ibrahim <mojahedibrahim95@gmail.com>
2022-01-24 11:34:15 -0500

    fix issues with redirects and logout

---
+++ logIn.js
@@ -0,0 +1,20 @@
+$(document).ready(function () {
+
+    $('#loginbtn').on('click', function () {
+        var userInputValue = $('#username').val();
+        var passwordInputValue = $('#password').val();
+        //var login is the string that will be stored in $_POST['action']
+        var login = 'login';
+        if (userInputValue.length > 0 && passwordInputValue.length > 0) {
+            $.post("index.php", { action: login, username: userInputValue, password: passwordInputValue }, function (loggedIn) {
+                $('body').html(loggedIn);
+            });
+        }else{
+            console.log("Please fill both fields")
+        }
+    });
+
+
+
+
+});
---
+++ signUp.php
@@ -0,0 +1,44 @@
+<?php
+session_start();
+require_once("./MyFirstClass.php");
+require_once("./Validator.php");
+$class = new MyFirstClass;
+$validator = new Validator;
+$users = $class->getUsers();
+if (isset($_POST['action'])) {
+    if ($validator->areRequiredFieldsSet($_POST)) {
+        if ($validator->isValidUsername($_POST['username'])) {
+            $username = $_POST['username'];
+        }
+        if ($validator->isValidfname($_POST['fname'])) {
+            $fname = $_POST['fname'];
+        }
+        if ($validator->isValidlname($_POST['lname'])) {
+            $lname = $_POST['lname'];
+        }
+        if ($validator->isValidpassword($_POST['password'])) {
+            $password = $_POST['password'];
+        }
+        if ($validator->isValidphone($_POST['phone'])) {
+            $phone = $_POST['phone'];
+        }
+        if ($validator->isValidemail($_POST['email'])) {
+            $email = $_POST['email'];
+        }
+        switch ($_POST['action']) {
+            case 'submitUser':
+                if ($class->addNewUser($_POST['username'], $_POST['fname'], $_POST['lname'], $_POST['email'], $_POST['phone'], $_POST['password'])) {
+                    header("location: ./toDoList.php");
+                }
+                break;
+        }
+    } else {
+        echo false;
+    }
+} else {
+    if (isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true) {
+        header("location: ./toDoList.php");
+        exit;
+    }
+    include("./signUpform.tpl.php");
+}
